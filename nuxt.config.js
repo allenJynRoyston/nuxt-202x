028bb48bd382b8cdb2b85159b98f570a14b59592
Allen Royston <allen.acr@gmail.com>
2020-09-01 08:44:06 +0100

    gzip, pwa setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,37 @@
 export default {
+  modules: [
+    '@nuxtjs/pwa',
+    'nuxt-compress'
+  ],   
+  "nuxt-compress": {
+    gzip: {
+      cache: true
+    },
+    brotli: {
+      threshold: 10240
+    }
+  },  
+  pwa: {
+    manifest: {
+      name: 'Jyn Labs',
+      short_name: 'Jyn Labs',
+      description: "Code, life and sex.",
+      lang: 'en',
+      theme_color: "#fff",
+      orientation: "portrait-primary",
+      useWebmanifestExtension: false
+    },    
+    icon: {
+      source: 'static/webmanifest/icon.png'
+    }
+  },  
   head: {
     link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'favicon/favicon.ico?v1',
+      },      
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto+Slab&display=swap'
